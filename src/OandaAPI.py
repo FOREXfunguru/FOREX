22b309b48fc68622eecc5af19d1dce9e2bc0593a
Ernesto Lowy <ernesto@ebi.ac.uk>
2017-02-28 14:11:40 +0000

    add changes

--- src/OandaAPI.py
+++ src/OandaAPI.py
@@ -12,6 +12,7 @@ from pandas.tseries.offsets import BDay
 from matplotlib.finance import volume_overlay
 from datetime import datetime,timedelta
 import datetime
+import pytz
 from pandas.stats.tests.common import start
 
 class Candle(object):
@@ -38,8 +39,7 @@ class Candle(object):
         self.volume=volume
         if complete not in [True, False] : raise Exception(("complete %s is not valid. Complete should be True or False")) % complete
         self.complete=complete
-        self.formation=formation
-        
+        self.formation=format
         
 class MidPointCandle(Candle):
     '''
@@ -81,7 +81,7 @@ class BidAskCandle(Candle):
     
     def __init__(self, representation=None, openBid=None, openAsk=None, highBid=None, highAsk=None, 
                  lowBid=None, lowAsk=None, closeBid=None, closeAsk=None, upper_wick=None, lower_wick=None
-                 ,colour=None):
+                 ,colour=None,perc_body=None,perc_uwick=None,perc_lwick=None):
         Candle.__init__(self,representation)
         self.openBid=openBid
         self.openAsk=openAsk
@@ -94,6 +94,9 @@ class BidAskCandle(Candle):
         self.colour=colour
         self.upper_wick=upper_wick
         self.lower_wick=lower_wick
+        self.perc_body=perc_body
+        self.perc_uwick=perc_uwick
+        self.perc_lwick=perc_lwick
     
     def set_candle_features (self):
         '''
@@ -121,7 +124,9 @@ class BidAskCandle(Candle):
             upper=self.openBid
             lower=self.closeBid
         else:
-            raise Exception("undefined colour")
+            self.colour="undefined"
+            upper=self.openBid
+            lower=self.closeBid
         
         self.upper_wick=self.highBid-upper
         self.lower_wick=lower-self.lowBid
@@ -155,6 +160,10 @@ class BidAskCandle(Candle):
         perc_uwick=(self.upper_wick*100)/height
         perc_lwick=(self.lower_wick*100)/height
         
+        self.perc_body=perc_body
+        self.perc_uwick=perc_uwick
+        self.perc_lwick=perc_lwick
+        
         if perc_body<35 and perc_lwick>60 and self.colour=="green":
             self.representation="hammer"
         elif perc_body<35 and perc_lwick>60 and self.colour=="red":
@@ -185,6 +194,40 @@ class BidAskCandle(Candle):
             out_str+="%s:%s " %(attr, value)
         return out_str
             
+class BiCandle:
+    '''
+    Constructor
+    
+    This class represents any combination of two candles
+
+    Class variables
+    ---------------
+    
+    candleA: BidAskCandle object
+             First candle in the pair
+    candleB: BidAskCandle object
+             Second candle in the pair
+    '''
+    def __init__(self, candleA, candleB):
+        self.candleA=candleA
+        self.candleB=candleB
+        
+    def is_engulfing(self):
+        '''
+        Does candleB engulfs candleA?. Engulfing happens when candleB body engulfs the whole
+        candleA (including the body and the wicks)
+        
+        Returns
+        ------
+        True or False
+        '''
+        
+        if self.candleB.openBid>self.candleA.highBid and self.candleB.closeBid<self.candleA.lowBid:
+            return True
+        else:
+            return False
+
+
 class OandaAPI(object):
     '''
     Class representing the content returned by a GET request to Oanda's REST API
@@ -287,6 +330,34 @@ class Reversal(object):
     Class representing a potential price reversal
     '''
     
+    def __is_market_closed(self,dt):
+        '''
+        Private method to check if the datetime object falls within a period when marked it closed
+        
+        dt  datetime object
+        
+        Returns
+        -------
+        Boolean    True is market is closed
+        
+        '''
+        if (dt.weekday()==4 and dt.time()==datetime.time(22,0,0)):
+            return True
+        elif (dt.month==12 and dt.day==24 and dt.time()>=datetime.time(22,0,0)):
+            return True
+        elif (dt.month==12 and dt.day==25):
+            return True
+        elif (dt.month==12 and dt.day==26 and dt.time()<datetime.time(22, 0, 0)):
+            return True
+        elif (dt.month==12 and dt.day==31 and dt.time()>=datetime.time(22, 0, 0)):
+            return True
+        elif (dt.month==1 and dt.day==1 and dt.time()<datetime.time(22, 0, 0)):
+            return True
+        elif (dt.weekday()==5 or (dt.weekday()==6 and dt.time()<datetime.time(22, 0, 0))):
+            return True
+        else:
+            return False
+
     def __offset_closed_marked(self,start,end,freq,delta,dir):
         '''
         This private function will check a period between 2 dates and will extend start/end depending on a particular date falling
@@ -303,30 +374,89 @@ class Reversal(object):
         dir: str, 'L'/'R'
             Direction of interval extension. 
         '''
-        
+        '''
         for d in pd.date_range(start=start,end=end, freq=freq):
-            if (d.weekday()==4 and d.time()==datetime.time(22,0,0)) or d.weekday()==5 or (d.weekday()==6 and d.time()<datetime.time(22, 0, 0)):
+            if self.__is_market_closed(d)==True:
                 if dir=='L':
                     start=start-delta
                 elif dir=='R':
                     end=end+delta
                 if dir=='L':
-                    while (start.weekday()==4 and start.time()==datetime.time(22,0,0)) or start.weekday()==5 or (start.weekday()==6 and start.time()<datetime.time(22, 0, 0)):
+                    while (start.weekday()==4 and start.time()>=datetime.time(21,0,0)) or start.weekday()==5 or (start.weekday()==6 and start.time()<datetime.time(21, 0, 0)):
                         start=start-delta
                 elif dir=='R':
-                    while (end.weekday()==4 and end.time()==datetime.time(22,0,0)) or end.weekday()==5 or (end.weekday()==6 and end.time()<datetime.time(22, 0, 0)):
+                    while (end.weekday()==4 and end.time()>=datetime.time(22,0,0)) or end.weekday()==5 or (end.weekday()==6 and end.time()<datetime.time(21, 0, 0)):
+                        end=end+delta
+        '''
+        for d in pd.date_range(start=start,end=end, freq=freq):
+            if self.__is_market_closed(d)==True:
+                if dir=='L':
+                    start=start-delta
+                elif dir=='R':
+                    end=end+delta
+                if dir=='L':
+                    while (self.__is_market_closed(start)):
+                        start=start-delta
+                elif dir=='R':
+                    while (self.__is_market_closed(end)):
                         end=end+delta
         if dir=='L':
              return start
         elif dir=='R':
-             return end 
+             return end
+    
+    def __check_correctness(self,obs_len,exp_len,exp_ic,obs_ic):
+        '''
+        Function to check if the candlelist returned by the Oanda API is correct
+        in terms of length and indecision candle
+        
+        Parameters
+        ----------
+        obs_len: int, Required
+                 Observed list length of the candlelist
+        exp_len: int, Required
+                 Expected list length as determined by the number_pre+number_post+ic
+        exp_ic: datetime object, Required
+                 Indecision candle as requested in the ic_candle class member of the Reversal object
+        obs_ic: datetime object, Required
+                 Observed Indecision candle.       
+        '''
+        
+        if obs_len!=exp_len:
+            raise Exception("candlelist length is not correct for %s!\nexpected: %d\nobserved: %d" % (exp_ic,exp_len,obs_len))
+        
+        if exp_ic!=obs_ic:
+            raise Exception("ind. candle  is not correct:\n expected %s\nobserved %s!" % (exp_ic,obs_ic))
+     
+    def __is_dst(self,timezone,time):
+         '''
+         Function to know if a particular candle time is in the daylight savings period
+         
+         Parameters
+        ----------
+         timezone: str, Required
+                   i.e. 'Europe/London','America/New_York'
+         time: datetime object 
+         
+         Returns
+         -------
+         Boolean, True means that the datetime is in the dst scheme
+         
+         '''
+         timezone=pytz.timezone(timezone)
+         dst_candle=timezone.localize(time, is_dst=None)
+         
+         return bool (dst_candle.dst())
+ 
 
-    def __init__(self, ic,outcome,number_pre,number_post,instrument,granularity='D',pre_candles=None,post_candles=None):
+    def __init__(self,type,ic,outcome,number_pre,number_post,instrument,granularity='D',pre_candles=None,post_candles=None):
         '''
         Constructor
 
          Class variables
         ---------------
+        type: str, [bullish or bearish]
+             Type of reversal
         ic : str
              String representing the time of the indecision candle
         outcome : boolean
@@ -341,9 +471,12 @@ class Reversal(object):
              Timeframe to use (i.e. D, H12)
         pre_candles : list, optional
              List of candles before the possible reversal (before the IC)
+        ic_candle : Candle object, optional
+             Indecission candle
         post_candles : list, optional
-             List of candles after the possible reversal (after the IC)
+             List of candles after the possible reversal (after the IC)        
         '''
+        self.type=type
         self.ic=ic
         self.outcome=outcome
         self.number_pre=number_pre
@@ -352,6 +485,9 @@ class Reversal(object):
         self.granularity=granularity
         self.pre_candles=pre_candles
         self.post_candles=post_candles
+        
+        if type!="bullish" and type!="bearish":
+            raise Exception("type of candle %s is not valid for %s! Valid types are bullish or bearish" % (ic,type))
         
         delta=""
         freq=""
@@ -368,39 +504,87 @@ class Reversal(object):
         
         candle = pd.datetime.strptime(ic, "%Y-%m-%d %H:%M:%S")
         
+        delta_10min=timedelta(minutes=10) 
+        
+        if self.__is_market_closed(candle)==True:
+            raise Exception("IC %s cannot be used!. Market is closed"% ic)
+
         start=candle-self.number_pre*delta
         end=candle+self.number_post*delta
         
         start=self.__offset_closed_marked(start,candle,freq,delta,'L')
         end=self.__offset_closed_marked(candle+delta,end,freq,delta,'R')
+                
+        l_lnd=[self.__is_dst('Europe/London',x) for x in [start,candle,end]]
+        l_ny=[self.__is_dst('America/New_York',x) for x in [start,candle,end]]
         
-        oanda=OandaAPI(url='https://api-fxtrade.oanda.com/v1/candles?',instrument=self.instrument,granularity=self.granularity,alignmentTimezone='Europe/London',dailyAlignment=22,start=start.isoformat(),end=end.isoformat())
+        if l_lnd!=l_ny:
+            raise Exception("Discrepancy in the DST. %s remove IC" % ic)
+        elif len(set(l_lnd))>1 or len(set(l_ny)) > 1:
+            raise Exception("Discrepancy in the DST. %s remove IC" % ic)
+        
+        '''correct for dst'''
+        dailyAlignment=22
+        
+        if list(set(l_lnd))[0]==True:
+            dailyAlignment+=1
+        
+        oanda=OandaAPI(url='https://api-fxtrade.oanda.com/v1/candles?',instrument=self.instrument,granularity=self.granularity,alignmentTimezone='Europe/London',dailyAlignment=dailyAlignment,start=start.isoformat(),end=(end+delta_10min).isoformat())
         
         candlelist=oanda.fetch_candleset()
         
+        newlist = [x for x in candlelist if self.__is_market_closed(x.time)==False]
+        
+        for c in newlist:
+            c.set_candle_features()
+            c.set_candle_formation()
+ 
         '''
         check if candlelist is correct
         '''
+        
         total=number_pre+number_post+1
-        midle=number_pre+1
-        if total!=len(candlelist):
-            raise Exception("candlelist length was not expected!")
-        
-        
+        middle=number_pre+1
+       
+        self.__check_correctness(obs_len=len(newlist),exp_len=total,exp_ic=candle,obs_ic=newlist[middle-1].time)   
 
-        for c in candlelist:
-            c.set_candle_features()
-            c.set_candle_formation()
-            print(c)
+        middle=number_pre+1
+        self.pre_candles=newlist[:number_pre]
+        self.ic_candle=newlist[middle-1]
+        self.post_candles=newlist[middle:]
         
-        
-        
+    def getwick_differential(self,candlelist,select):
         '''
-        if colors:
-            self.colors=colors
-        else:
-            self.colors=[]
-            for c in candles:
-                c.set_candle_features
-                self.colors.append(c.colour)
-         ''' 
+        Method to get the difference in the price between the current upper/lower wick and the previous one
+        
+        Parameters
+        ----------
+        candlelist: list
+                    List of candles
+        select : str, [upper, lower] required
+                 Calculate the different between highs (upper), lows (lower)
+        
+        Return
+        ------
+        List of floats with the difference between the highs/lows
+        '''
+        
+        p_candle=0.0
+        l=[]
+        
+        for k,i in enumerate(candlelist):
+            val=0.0
+            if select=="upper":
+                val=i.highBid
+            elif select=="lower":
+                val=i.lowBid
+            else:
+                raise Exception("select argument %s is not recognised" % select)         
+            
+            if(k==0):
+                p_candle=float(val)
+            else:
+                c_candle=float(val)-p_candle
+                l.append(c_candle)
+                p_candle=float(val)
+        return l
--- src/scripts/analyze_reversal.py
+++ src/scripts/analyze_reversal.py
@@ -1,14 +1,74 @@
+from __future__ import division
 from OandaAPI import OandaAPI
 from OandaAPI import Reversal
+from OandaAPI import BiCandle
 import pandas as pd
 from pandas.tseries.offsets import BDay
 import logging
+import warnings
 
 logging.basicConfig(level=logging.INFO)
 logging.info("Program started")
 
-ic="2016-10-11 22:00:00"
 
-r=Reversal(ic=ic,outcome=True,number_pre=3,number_post=3,instrument='AUD_USD',granularity='D')
+xls_file = pd.ExcelFile('/Users/ernesto/Google_Drive/MONEDAS/backtesting/data_collection_29012017stripped.xlsx')
 
+df = xls_file.parse('Sheet1')
+
+newline=""
+out=open('/Users/ernesto/projects/FOREX/out.txt','w')
+out.write("instrument\tgranularity\tdate time\ttype\toutcome\tengulfing?\tdiffm1_u\tdiffm1_l\tdiffp1_u\tdiffp1_l\t[perc_uwick:perc_body:perc_lwick]p1\tcolors_pre_[m1,ic]\tcolors_post_[ic,p1]\tformation_pre_[m1,ic]\tformation_post_[ic,p1]\tavg_volume_pre\tvolume_pre_[m1,ic]\tvolume_post_[ic,p1]\n")
+
+for index, row in df.iterrows():
+    warnings.warn("INFO: Analysing %s:%s" % (row['pair'],row['date']))
+    candlelist_post=[]
+    candlelist_pre=[]
+    if row['outcome']==1: outcome=True
+    if row['outcome']==0: outcome=False
+    newline="%s\t%s\t%s\t%s\t%s" % (row['pair'], row['granularity'], row['date'], row['type'], str(outcome))
+    ic=row['date']
+    r=Reversal(ic=str(row['date']),outcome=True,number_pre=3,number_post=3,instrument=row['pair'],granularity=row['granularity'],type=row['type'])
+    candlelist_post.append(r.ic_candle)
+    candlelist_post.append(r.post_candles[0])
+    candlelist_pre.append(r.pre_candles[-1])
+    candlelist_pre.append(r.ic_candle)
+    bc=BiCandle(candleA=candlelist_post[0],candleB=candlelist_post[1])
+    newline= newline+"\t"+ str(bc.is_engulfing()) +"\t"
+    listpost_upper=r.getwick_differential(candlelist_post,'upper')
+    listpost_lower=r.getwick_differential(candlelist_post,'lower')
+    listpre_upper=r.getwick_differential(candlelist_pre,'upper')
+    listpre_lower=r.getwick_differential(candlelist_pre,'lower')
+    for i, j in zip(listpre_upper, listpre_lower):
+        newline= newline+"%f\t%f\t" % (i,j)
+    for i, j in zip(listpost_upper, listpost_lower):
+        newline= newline+"%f\t%f" % (i,j)
+    
+    newline=newline+"\t[%f:%f:%f]" %(candlelist_post[1].perc_uwick,candlelist_post[1].perc_body,candlelist_post[1].perc_lwick)
+    
+    colors_pre="{0}"
+    colors_pre=",".join(colors_pre.format(c.colour) for c in candlelist_pre)
+    colors_post="{0}"
+    colors_post=",".join(colors_post.format(c.colour) for c in candlelist_post)
+    
+    newline=newline+"\t"+colors_pre+"\t"+colors_post
+    
+    formation_pre="{0}"
+    formation_pre=",".join(formation_pre.format(c.representation) for c in candlelist_pre)
+    formation_post="{0}"
+    formation_post=",".join(formation_post.format(c.representation) for c in candlelist_post)
+    newline=newline+"\t"+formation_pre+"\t"+formation_post+"\t"
+    
+    sum_pre_candles=sum(c.volume for c in r.pre_candles)
+    avg_vol_pre=sum_pre_candles/len(r.pre_candles)
+    
+    volume_pre="{0}"
+    volume_pre=",".join(volume_pre.format(c.volume) for c in candlelist_pre)
+    volume_post="{0}"
+    volume_post=",".join(volume_post.format(c.volume) for c in candlelist_post)
+    
+    newline=newline+ '%.2f' % avg_vol_pre+"\t"+volume_pre+"\t"+volume_post+"\n"
+    
+    out.write(newline)
+    
+out.close()
 logging.info("Done!")
