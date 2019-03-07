1763a9cf6d4778bc516e27138dd41dff52cc2098
Ernesto Lowy <ernestolowy@gmail.com>
2019-03-07 12:28:26 +0000

    Print support for python3

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ logging.info("Program started")
 
 #loanda=OandaAPI(url='https://api-fxtrade.oanda.com/v1/candles?',instrument='EUR_USD',granularity='H8',alignmentTimezone='Europe/London',dailyAlignment=22,start='2016-09-13T06:00:00',count=40)
 oanda=OandaAPI(url='https://api-fxtrade.oanda.com/v1/candles?',instrument='EUR_USD',granularity='H8',alignmentTimezone='Europe/London',dailyAlignment=22,start='2016-10-24T22:00:00',count=1)
-print oanda.print_url()
+print(oanda.print_url())
 
 candlelist=oanda.fetch_candleset()
 
