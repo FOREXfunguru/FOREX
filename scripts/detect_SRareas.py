4a704a9c773cecf55bcabbfe655fb401cafa88c3
elowy01 <ernestolowy@gmail.com>
2020-01-17 17:05:16 +0000

    finalyzing detectSRareas

--- scripts/detect_SRareas.py
+++ scripts/detect_SRareas.py
@@ -1,5 +1,8 @@
 import argparse
 import numpy as np
+from utils import *
+import pdb
+
 from Pattern.counter import Counter
 
 
@@ -12,21 +15,36 @@ parser.add_argument('--start', required=True, help='Start time to detect SR area
 
 args = parser.parse_args()
 
-increment_price=0.002
+file = open('out_scores.txt','w')
+
+
+file.write("#price\tn_bounces\ttot_score\tscore_per_bounce\n")
+
+increment_price=0.003
 # the increment of price in number of pips is double the hr_extension
-for p in np.arange(0.68612, 0.75548, increment_price):
-    print("Price: {0}".format(p))
+for p in np.arange(1.19000, 1.72016, increment_price):
+
+    print("Processing S/R at {0}".format(round(p,4)))
+    #each of 'p' will become a S/R that will be tested for bounces
+    #set entry to price+30pips
+    entry=add_pips2price(args.instrument,p,30)
+    #set S/L to price-30pips
+    SL=substract_pips2price(args.instrument,p,30)
+
     c = Counter(
-        id='EUR_GBP 13AUG2019D',
+        id='detect_sr',
         start=args.start,
-        pair='EUR_GBP',
+        pair=args.instrument,
         timeframe='D',
         type='short',
-        period=1000,
+        period=5000,
+        entry=entry,
         SR=p,
+        SL=SL,
         RR=1.5,
-        png_prefix='data/tmp/test')
+        png_prefix='/Users/ernesto/projects/FOREX/CT/detect_srareas/test.{0}'.format(p))
+    file.write("{0}\t{1}\t{2}\t{3}\n".format(round(p,5),len(c.bounces.plist),c.total_score,round(c.total_score/len(c.bounces.plist),2)))
 
-print("h")
+file.close()
 
 
