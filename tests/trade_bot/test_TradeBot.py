d7cbf22e2bbf6e647fa8570921ed19376a751c17
elowy01 <ernestolowy@gmail.com>
2020-04-21 23:56:49 +0100

    finishing trade_bot

--- tests/trade_bot/test_TradeBot.py
+++ tests/trade_bot/test_TradeBot.py
@@ -3,6 +3,7 @@ from trade_bot.trade_bot import TradeBot
 import pytest
 import glob
 import os
+import datetime
 
 @pytest.fixture
 def clean_tmp():
@@ -24,6 +25,18 @@ def tb_object():
     )
     return tb
 
+@pytest.fixture
+def tb_object1():
+    '''Returns TradeBot object'''
+    tb = TradeBot(
+        pair='EUR_GBP',
+        timeframe='D',
+        start='2017-06-11 22:00:00',
+        end='2017-06-15 22:00:00',
+        settingf="../../data/settings.ini"
+    )
+    return tb
+
 def test_run(tb_object):
     """
     Check 'run' function
@@ -35,6 +48,15 @@ def test_calc_SR(tb_object, clean_tmp):
     Check 'calc_SR' function
     """
     harealst = tb_object.calc_SR()
+
+    # check the length of HAreaList.halist
+    assert len(harealst.halist) == 1
+
+def test_calc_SR1(tb_object, clean_tmp):
+    """
+    Check 'calc_SR' function
+    """
+    harealst = tb_object.calc_SR(datetime.datetime(2017, 6, 13, 22, 0))
 
     # check the length of HAreaList.halist
     assert len(harealst.halist) == 1
