f2e356de9d476a605de2d92c71e4a3496e18a358
Ernesto Lowy <ernestolowy@gmail.com>
2023-04-11 20:23:46 +0100

    fixing test with pickled H8 candlelist

--- tests/data/create_pickled_data.py
+++ tests/data/create_pickled_data.py
@@ -4,12 +4,20 @@ from utils import DATA_DIR
 from forex.pivot import PivotList
 from api.oanda.connect import Connect
 
-# pickle CandleList
+# pickle CandleList Daily
 conn = Connect(
         instrument="AUD_USD",
         granularity='D')
 clO = conn.query('2010-11-16T22:00:00', '2020-11-19T22:00:00')
 clO.pickle_dump(f"{DATA_DIR}/clist_audusd_2010_2020.pckl")
+
+# pickle CandleList H8
+conn = Connect(
+        instrument="AUD_USD",
+        granularity='H8')
+clO = conn.query('2021-01-03T22:00:00', '2021-12-31T14:00:00')
+clO.pickle_dump(f"{DATA_DIR}/clist.AUDUSD.H8.2021.pckl")
+
 
 # pickle Segment
 clObj = CandleList.pickle_load(f"{DATA_DIR}/clist_audusd_2010_2020.pckl")
