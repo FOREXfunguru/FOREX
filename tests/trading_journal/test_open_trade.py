4bba2523f68ee805fbfc3543d41988c023ed53b4
Ernesto Lowy <ernestolowy@gmail.com>
2023-11-19 10:31:37 +0000

    WIP

--- tests/trading_journal/test_open_trade.py
+++ tests/trading_journal/test_open_trade.py
@@ -1,20 +1,22 @@
 import pytest
+import pdb
 
 from trading_journal.open_trade import UnawareTrade
 
-trade_details = {"start": "2020-02-19T21:00:00",
-                 "pair": "EUR_GBP",
+trade_details = {"start": "2021-02-19T21:00:00",
+                 "pair": "AUD_USD",
                  "type": "long",
-                 "timeframe": "D",
+                 "timeframe": "H8",
                  "entry": 0.83585,
                  "SL": 0.82467,
                  "RR": 0.86032}
 
 
 @pytest.fixture
-def unaware_object(clO_pickled):
-    trade_details["clist"] = clO_pickled
-    return UnawareTrade(**trade_details)
+def unaware_object(clO_H8_pickled, clO_D_2021pickled):
+    trade_details["clist"] = clO_H8_pickled
+    return UnawareTrade(clist_tm=clO_D_2021pickled,
+                        **trade_details)
 
 
 def test_instantiation(unaware_object):
--- trading_journal/open_trade.py
+++ trading_journal/open_trade.py
@@ -1,3 +1,5 @@
+import pdb
+
 from trading_journal.trade import Trade
 from forex.candle import CandleList
 
@@ -17,10 +19,22 @@ class UnawareTrade(Trade):
             candle_number: number of candles against the trade to consider
             clist_tm: CandleList used for trade management
         """
-        self.clist_tm = clist_tm
         self.candle_number = candle_number
         super().__init__(**kwargs)
-    
+        self.clist_tm = clist_tm
+
+    @property
+    def clist_tm(self):
+        return self._clist_tm
+
+    @clist_tm.setter
+    def clist_tm(self, clist: CandleList):
+        pdb.set_trace()
+        if self.start > clist.candles[-1].time:
+            raise ValueError("Incorrect 'clist_tm'. Trade start "
+                             "does not match the end of 'clist_end'")
+        self._clist_tm = clist
+
     def check_if_against(self):
         """Function to check if middle_point values are
         agaisnt the trade
@@ -35,15 +49,19 @@ class UnawareTrade(Trade):
         """
         count = 1
         for d in self.gen_datelist():
+            pdb.set_trace()
             cl = self.clist[d]
             if cl is None:
                 if connect is True:
                     cl = self._fetch_candle(d=d)
                 if cl is None:
                     continue
-            UnawareTrade.middle_point.append(cl.middle_point())
+            cl_tm = self.clist_tm[d]
+            if cl_tm is not None:
+                pdb.set_trace()
+                UnawareTrade.middle_point.append(cl.middle_point())
             if count % self.candle_number == 0:
                 self.check_if_against()
             count += 1
-            
+         
             print(cl)
