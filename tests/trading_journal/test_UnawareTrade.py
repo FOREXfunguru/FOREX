fb6323841b89f8576d0907d3167fad90d773950a
Ernesto Lowy <ernestolowy@gmail.com>
2024-01-26 20:54:39 +0000

    Modify tests for UnawareTrade objects

--- tests/trading_journal/test_UnawareTrade.py
+++ tests/trading_journal/test_UnawareTrade.py
@@ -1,10 +1,8 @@
 import pytest
-import datetime
 
 from trading_journal.open_trade import UnawareTrade
 
 from data_for_tests import (trades1,
-                            last_times,
                             trades_entered,
                             trades_outcome)
 
@@ -55,67 +53,6 @@ def test_init_clist():
     )
     assert len(td.clist.candles) == 4104
 
-
-trade_data1 = [(*trade, pip) for trade, pip in zip(trades1, last_times)]
-
-@pytest.mark.parametrize("start,type,SR,SL,TP,entry,lasttime", trade_data1)
-def test_get_lasttime(start, type, SR, SL, TP, entry, lasttime, clO_pickled):
-    """Check function get_lasttime"""
-    t = UnawareTrade(
-        id="test",
-        start=start,
-        pair="AUD_USD",
-        timeframe="D",
-        type=type,
-        SR=SR,
-        SL=SL,
-        TP=TP,
-        entry=entry,
-        clist=clO_pickled)
-
-    assert t.get_lasttime() == lasttime
-
-@pytest.mark.parametrize(
-    "start," "type," "SR," "SL," "TP," "entry," "lasttime",
-    [
-        (
-            "2017-12-10 22:00:00",
-            "long",
-            0.74986,
-            0.74720,
-            0.76521,
-            0.75319,
-            datetime.datetime(2017, 6, 1, 21, 0),
-        ),
-        (
-            "2017-03-21 22:00:00",
-            "short",
-            0.77103,
-            0.77876,
-            0.73896,
-            0.76717,
-            datetime.datetime(2016, 4, 19, 21, 0),
-        ),
-    ],
-)
-def test_get_lasttime_with_pad(start, type, SR, SL, TP, entry, lasttime, clO_pickled):
-    """Check function get_lasttime"""
-    t = UnawareTrade(
-        id="test",
-        start=start,
-        pair="AUD_USD",
-        timeframe="D",
-        type=type,
-        SR=SR,
-        SL=SL,
-        TP=TP,
-        entry=entry,
-        clist=clO_pickled,
-    )
-
-    assert t.get_lasttime(pad=30) == lasttime
-
-
 # add a non initialised trade
 trades_for_test_initialise = trades1[:]
 trades_for_test_initialise.append(("2018-12-23 22:00:00", "long", 0.70112, 0.69637, 0.72756, 0.70895))
@@ -144,32 +81,6 @@ def test_initialise(start, type, SR, SL, TP, entry, entry_data, clO_pickled):
     assert t.entered == entry_data[0]
     if hasattr(t, "entry_time"):
         assert t.entry_time == entry_data[1]
-
-trades_for_entry_onrsi = trades1[:]
-trades_for_entry_onrsi.append(("2017-07-25 22:00:00", "short", 0.79743, 0.80577, 0.77479, 0.79343))
-
-is_on_rsi = [False, False, False, False, True]
-trade_data3 = [(*trade, pip) for trade, pip in zip(trades_for_entry_onrsi, is_on_rsi)]
-@pytest.mark.parametrize("start,type,SR,SL,TP,entry,entry_onrsi", trade_data3)
-def test_is_entry_onrsi(start, type, SR, SL, TP, entry, entry_onrsi, clO_pickled):
-    """Test is_entry_onrsi function"""
-    t = UnawareTrade(
-        id="test",
-        start=start,
-        pair="AUD_USD",
-        timeframe="D",
-        type=type,
-        SR=SR,
-        SL=SL,
-        TP=TP,
-        entry=entry,
-        clist=clO_pickled,
-        clist_tm=clO_pickled
-    )
-    newclist = t.clist
-    newclist.calc_rsi()
-    t.clist = newclist
-    assert entry_onrsi == t.is_entry_onrsi()
 
 
 trade_data1 = [(*trade, pip) for trade, pip in zip(trades_entered, trades_outcome)]
